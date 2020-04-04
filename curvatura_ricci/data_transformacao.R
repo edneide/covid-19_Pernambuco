3df2c7e61ff264d590bca13817782b1e9253cda1
Edneide Ramalho <edneide.ramalho@gmail.com>
2020-04-03 22:55:11 -0300

    Create data_transformacao.R

---
+++ curvatura_ricci/data_transformacao.R
@@ -0,0 +1,15 @@
+# Transformando datas
+library(dplyr)
+library(readr)
+forman_ricci_brasil <- read_csv("~/Documents/GitHub/covid-19_Pernambuco/curvatura_ricci/forman_ricci_brasil.csv")
+
+forman_ricci_brasil$periodo = as.Date.character(forman_ricci_brasil$periodo, format = "%d %b")
+class(forman_ricci_brasil$periodo)
+
+write.csv(forman_ricci_brasil, "forman_ricci_brasil2.csv")
+
+# Mundial 
+forman_ricci_mundial <- read_csv("~/Documents/GitHub/covid-19_Pernambuco/curvatura_ricci/forman_ricci_mundial.csv")
+forman_ricci_mundial$periodo = as.Date.character(forman_ricci_mundial$periodo, format = "%d %b")
+class(forman_ricci_mundial$periodo)
+write.csv(forman_ricci_mundial, "forman_ricci_mundial2.csv")
