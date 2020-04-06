eacf10513616275e2ac465a3520e1436a6f07001
Edneide Ramalho <edneide.ramalho@gmail.com>
2020-04-06 19:20:02 -0300

    Update recodificando_variaveis.R

--- recodificando_variaveis.R
+++ recodificando_variaveis.R
@@ -1,10 +1,8 @@
 # Carregando a base de dados limpa
 library(readr)
-df <- read_delim("~/Google Drive/Coronavirus/IRRD/planilhas de pe/06-04-2020/recife_att_DOTS_2020-04-06_18-42.csv", 
-                                               ";", escape_double = FALSE, trim_ws = TRUE)
-
-
-
+df <- read_delim("~/Google Drive/Coronavirus/IRRD/planilhas de pe/06-04-2020/recife_att_2020-04-06_19-14.csv", 
+                                          ";", escape_double = FALSE, trim_ws = TRUE)
+#View(recife_att_2020_04_06_19_14)
 names(df)
 
 # Aplicando a função para padronizar o nome das variáveis
