f2e6e6c12b6845cd5e6463f42a5cc6be55fbc090
Edneide Ramalho <edneide.ramalho@gmail.com>
2020-05-04 06:22:23 -0300

    update

--- recodificando_variaveis.R
+++ recodificando_variaveis.R
@@ -1,10 +1,9 @@
 # Carregando a base de dados limpa
 library(readr)
-df <- read_delim("~/Google Drive/Coronavirus/IRRD/planilhas de pe/02-05-2020/recife_att_DOTS_2020-05-03_05-49.csv", 
+df <- read_delim("~/Google Drive/Coronavirus/IRRD/planilhas de pe/03-05-2020/recife_att_DOTS_2020-05-04_06-04.csv", 
                                                ";", escape_double = FALSE, trim_ws = TRUE)
-
-
 names(df)
+
 # Aplicando a função para padronizar o nome das variáveis
 library(janitor)
 de <- janitor::clean_names(df)
