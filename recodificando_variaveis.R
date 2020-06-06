ec48102b7f274e1c67abd589a29f233289ab3ab3
Edneide Ramalho <edneide.ramalho@gmail.com>
2020-06-06 09:35:36 -0300

    Update recodificando_variaveis.R

--- recodificando_variaveis.R
+++ recodificando_variaveis.R
@@ -2,7 +2,7 @@ setwd("~/Documents/GitHub/covid-19_Pernambuco")
 
 # Carregando a base de dados limpa
 library(readr)
-df <- read_delim("~/Google Drive/Coronavirus/IRRD/planilhas de pe/05-06-2020/bases_2020-06-06_00/recife_att_DOTS_2020-06-06_06-50.csv", 
+df <- read_delim("~/Google Drive/Coronavirus/IRRD/planilhas de pe/05-06-2020/bases_2020-06-06_01/recife_att_DOTS_2020-06-06_09-26.csv", 
                                                ";", escape_double = FALSE, trim_ws = TRUE)
 names(df)
 
