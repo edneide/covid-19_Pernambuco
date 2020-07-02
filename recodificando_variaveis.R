e5b578e79333b5d577cd156fc48e64fe469585a3
Edneide Ramalho <edneide.ramalho@gmail.com>
2020-07-01 23:29:10 -0300

    update

--- recodificando_variaveis.R
+++ recodificando_variaveis.R
@@ -1,9 +1,14 @@
 setwd("~/Documents/GitHub/covid-19_Pernambuco")
 
+
+
+hoje <- format(as.Date(today()), "%d-%m-%Y")
+
+
 # Carregando a base de dados limpa
 library(readr)
-df <- read_delim("~/Google Drive/Coronavirus/IRRD/planilhas de pe/30-06-2020/bases/recife_att_DOTS.csv", 
-                                               ";", escape_double = FALSE, trim_ws = TRUE)
+df <- read_delim(paste0("~/Google Drive/Coronavirus/IRRD/planilhas de pe/", hoje,
+"/bases/recife_att_DOTS.csv"), ";", escape_double = FALSE, trim_ws = TRUE)
 names(df)
 
 # Aplicando a função para padronizar o nome das variáveis
