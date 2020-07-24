7dc177bc171e1d6ba6230b38253d33549f399c49
Edneide Ramalho <edneide.ramalho@gmail.com>
2020-07-23 23:47:22 -0300

    Update

--- recodificando_variaveis.R
+++ recodificando_variaveis.R
@@ -8,7 +8,7 @@ hoje <- format(as.Date(today()-1), "%d-%m-%Y")
 
 # Carregando a base de dados limpa
 library(readr)
-df <- read_delim(paste0("~/Google Drive/Coronavirus/IRRD/planilhas de pe/", hoje,
+df <- read_delim(paste0("~/Google Drive/Coronavirus/IRRD/planilhas de pe/julho/", hoje,
 "/bases/recife_att.csv"), ";", escape_double = FALSE, trim_ws = TRUE)
 names(df)
 
