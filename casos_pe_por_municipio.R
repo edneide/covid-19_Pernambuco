aec08ddddc5462f7f9d44f38bde93d72a655a523
Edneide Ramalho <edneide.ramalho@gmail.com>
2020-07-26 22:48:12 -0300

    update

--- casos_pe_por_municipio.R
+++ casos_pe_por_municipio.R
@@ -1,10 +1,12 @@
 #setwd("~/Google Drive/Coronavirus/IRRD/planilhas de pe/21-07-2020/")
+caminho <- "/Users/edneideramalho/Google Drive/Coronavirus/IRRD/planilhas de pe/Casos e óbitos por município - PE/"
 
 library(readr)
 
 # Casos 
 casos_total <- read.csv("https://raw.githubusercontent.com/JTeodomiro/covid19PE/master/PE_Cidades_DATA.csv")
 casos_srag <- read.csv("https://raw.githubusercontent.com/JTeodomiro/covid19PE/master/PE_Cidades_DATA_SRAG.csv")
+#casos_leve <- read.csv("")
 
 # Óbitos
 obitos_pe <- read.csv("https://raw.githubusercontent.com/JTeodomiro/covid19PE/master/Obitos_PE_Cidades.csv")
@@ -15,7 +17,7 @@ dim(casos_srag)
 
 library(lubridate)
 # Salvando em csv
-write.csv(casos_srag, paste0("covid_19_PE_SRAG_att_em_", today()-1, ".csv"))
-write.csv(obitos_pe, paste0("covid_19_PE_obitos_att_em_", today()-1, ".csv"))
-write.csv(casos_total,paste0("covid_19_PE_casos_total_att_em_", today()-1, ".csv"))
+write.csv(casos_srag, paste0(caminho,"covid_19_PE_SRAG.csv"))
+write.csv(obitos_pe, paste0(caminho, "covid_19_PE_obitos.csv"))
+write.csv(casos_total,paste0(caminho, "covid_19_PE_casos_total.csv"))
 
